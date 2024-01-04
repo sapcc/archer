123d457f84b17fd50d05b27c2adde19cee792c8e
Andrew Karpow <andrew.karpow@sap.com>
2024-01-04 11:59:57 -0500

    [f5] add routeDomain and vlan cleanup

--- internal/agent/f5/cleanup.go
+++ internal/agent/f5/cleanup.go
@@ -15,11 +15,15 @@
 package f5
 
 import (
+	"context"
 	"errors"
 	"fmt"
 
 	"github.com/gophercloud/gophercloud"
 	log "github.com/sirupsen/logrus"
+
+	"github.com/sapcc/archer/internal/config"
+	"github.com/sapcc/archer/internal/db"
 )
 
 // scanAndClean scans all selfips on all bigips and deletes them if they are not in the database
@@ -58,4 +62,94 @@ func (a *Agent) cleanOrphanSelfIPs() {
 		}
 	}
 	log.Debug("Finished CleanOrphanSelfIPs")
+}
+
+func (a *Agent) cleanOrphanRDsAndVLANs() error {
+	log.Debug("Running cleanOrphanRDsAndVLANs")
+
+	sql, args := db.Select("s.network_id", "ep.segment_id").
+		Join("endpoint e ON s.id = e.service_id").
+		Join("endpoint_port ep ON ep.endpoint_id = e.id").
+		From("service s").
+		Where("s.host = ?", config.Global.Default.Host).
+		Where("s.provider = 'tenant'").
+		MustSql()
+
+	rows, err := a.pool.Query(context.Background(), sql, args...)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	usedSegments := map[int]struct{}{}
+	for rows.Next() {
+		var networkID string
+		var segmentID int
+
+		if err = rows.Scan(&networkID, &segmentID); err != nil {
+			return err
+		}
+		usedSegments[segmentID] = struct{}{}
+		serviceSegment, err := a.neutron.GetNetworkSegment(networkID)
+		if err != nil {
+			return err
+		}
+		usedSegments[serviceSegment] = struct{}{}
+	}
+
+	// Print used segments
+	log.Debugf("cleanOrphanRDsAndVLANs: Used segments: %v", usedSegments)
+
+	for _, bigip := range a.bigips {
+		routeDomains, err := bigip.RouteDomains()
+		if err != nil {
+			return err
+		}
+
+		for _, routeDomain := range routeDomains.RouteDomains {
+			// Check that routeDomain starts with vlan-
+			var id int
+			if n, err := fmt.Sscanf(routeDomain.Name, "vlan-%d", &id); err != nil || n != 1 {
+				continue
+			}
+
+			// Check if routeDomain is used
+			if _, ok := usedSegments[id]; ok {
+				continue
+			}
+			log.WithField("host", bigip.GetHostname()).
+				Infof(" - found orphan routeDomain %s, deleting", routeDomain.Name)
+			if err := bigip.DeleteRouteDomain(routeDomain.Name); err != nil {
+				log.Warningf("- skipping due interdependency: %s", err.Error())
+			}
+		}
+	}
+
+	for _, bigip := range a.bigips {
+		vlans, err := bigip.Vlans()
+		if err != nil {
+			return err
+		}
+
+		for _, vlan := range vlans.Vlans {
+			// Check that routeDomain starts with vlan-
+			var segment int
+			if n, err := fmt.Sscanf(vlan.Name, "vlan-%d", &segment); err != nil || n != 1 {
+				continue
+			}
+
+			// Check if routeDomain is used
+			if _, ok := usedSegments[segment]; ok {
+				continue
+			}
+			log.WithField("host", bigip.GetHostname()).
+				Infof(" - Found orphan vlan %s, deleting", vlan.Name)
+			if err := bigip.DeleteVlan(vlan.Name); err != nil {
+				log.Error(err)
+			}
+		}
+	}
+
+	log.Debug("Finished cleanOrphanRDsAndVLANs")
+	return nil
 }
