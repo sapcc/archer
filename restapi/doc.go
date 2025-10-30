eb7a909a65f27fa51d590344c21793ed091498b3
Andrew Karpow <andrew.karpow@sap.com>
2025-10-30 19:42:25 +0100

    Bump swagger spec to 2.0.0 to designate incompatible attribute change

--- restapi/doc.go
+++ restapi/doc.go
@@ -145,7 +145,7 @@
 //	  https
 //	Host: localhost
 //	BasePath: /
-//	Version: 1.5.0
+//	Version: 2.0.0
 //	License: Apache 2.0 https://www.apache.org/licenses/LICENSE-2.0.html
 //	Contact: SAP SE / Converged Cloud https://sap.com
 //
